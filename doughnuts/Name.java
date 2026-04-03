9e7fa4fe2048df9f962a5e463ddef8989724b61c
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 00:16:48 +0100

    added already done files

---
+++ doughnuts/Name.java
@@ -0,0 +1,11 @@
+/*
+*A Program that prints my first name, last name and this year 
+*on seperate lines using only one print statement.
+*/
+
+public class Name{
+
+    public static void main(String[] args){
+    System.out.printf("First Name: %s%nLast Name: %s%nYear: %d%n", "Vincent", "Opurum", 2026);
+    }
+}
