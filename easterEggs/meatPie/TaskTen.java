71df6a09b24c87fec858415e94985552b6111b43
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 02:51:16 +0100

    added task ten

---
+++ easterEggs/meatPie/TaskTen.java
@@ -0,0 +1,10 @@
+/*
+* A Java program that takes a number and prints its double.
+*/
+public class TaskTen{
+    public static void main(String[] args){
+        
+        int number = 50;
+        System.out.printf("The double of %d is %d%n", number, number+number);
+    }
+}
