ff76ef6a7d8cd89abb4dd99950fbb8a0762d0e52
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 01:21:55 +0100

    added task 4

---
+++ easterEggs/meatPie/Difference.java
@@ -0,0 +1,13 @@
+/*
+* A Java program that takes two numbers and print their difference
+*/
+
+public class Difference{
+    public static void main(String[] args){
+        
+        int firstNum = 459;
+        int secondNum = 78;
+        int difference = firstNum - secondNum;
+        System.out.printf("The difference of %d and %d is %d%n", firstNum, secondNum, difference);
+    }
+}
--- easterEggs/meatPie/Sum.java
+++ easterEggs/meatPie/Sum.java
@@ -1,15 +1,12 @@
 /*
 * A Java program that takes two numbers and print their sum.
 */
-import java.util.Scanner;
+
 public class Sum{
     public static void main(String[] args){
         
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int firstNum = input.nextInt();
-        System.out.print("Enter second number: ");
-        int secondNum = input.nextInt();
+        int firstNum = 45;
+        int secondNum = 78;
         int sum = firstNum + secondNum;
         System.out.printf("The sum of %d and %d is %d%n", firstNum, secondNum, sum);
     }
