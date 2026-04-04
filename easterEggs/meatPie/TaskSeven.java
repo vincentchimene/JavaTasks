843af27180fe3012d61f1e23f529ebc2c26bd39b
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 02:28:27 +0100

    added task 7

---
+++ easterEggs/meatPie/TaskSeven.java
@@ -0,0 +1,9 @@
+/*
+* A Java program that takes a number and prints its square.
+*/
+public class TaskSeven{
+    public static void main(String[] args){
+        int number = 25;
+        System.out.printf("The square of %d is %d%n", number, number*number);
+    }
+}
