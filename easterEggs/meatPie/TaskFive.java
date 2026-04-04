5205f5c010eadda7f8f520061965709b40d71cd9
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 01:54:16 +0100

    added task 5

---
+++ easterEggs/meatPie/TaskFive.java
@@ -0,0 +1,13 @@
+/*
+* A Java program that takes two numbers and print their product
+*/
+
+public class TaskFive{
+    public static void main(String[] args){
+        
+        int firstNum = 53;
+        int secondNum = 78;
+        int product = firstNum * secondNum;
+        System.out.printf("The product of %d and %d is %d%n", firstNum, secondNum, product);
+    }
+}
