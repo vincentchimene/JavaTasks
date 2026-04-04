c8fd28e832d70797299dffe38df3dc3c18342f70
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 08:11:39 +0100

    adds task 13

---
+++ easterEggs/meatPie/TaskThirteen.java
@@ -0,0 +1,12 @@
+/*
+* A java program that declares a String variable called city,
+* assign it any city name, and prints:I live in [insert city].
+*/
+public class TaskThirteen{
+    public static void main(String[] args){
+
+        String city = "Port Harcourt";
+        System.out.printf("I live in %s.%n", city);
+    }
+
+}
