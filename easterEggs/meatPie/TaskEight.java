b8a40d71c522782dfc67cedbe32aa8bea613a5c8
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 02:37:15 +0100

    added task 8

---
+++ easterEggs/meatPie/TaskEight.java
@@ -0,0 +1,10 @@
+/*
+* A Java program that takes a number and prints its square.
+*/
+public class TaskEight{
+    public static void main(String[] args){
+        
+        int number = 25;
+        System.out.printf("The cube of %d is %d%n", number, number*number*number);
+    }
+}
