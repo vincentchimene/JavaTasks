5eef4cff41f0ac5577b8d29942f72bd8ab78e1e8
Vincent Opurum <vincentchimene@gmail.com>
2026-04-04 03:08:57 +0100

    adds Task 11

---
+++ easterEggs/meatPie/TaskEleven.java
@@ -0,0 +1,10 @@
+/*
+* A Java program that takes a number and prints its half.
+*/
+public class TaskEleven{
+    public static void main(String[] args){
+        
+        int number = 100;
+        System.out.printf("The half of %d is %d%n", number, number/2);
+    }
+}
