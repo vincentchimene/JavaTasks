6d92fba793482bef32b7ad43259148ad08f10028
Vincent Opurum <vincentchimene@gmail.com>
2026-04-09 00:09:40 +0100

    corrects the code to use integers instead of decimals

--- easterEggs/bitterKola/MultipleOfTheOther.java
+++ easterEggs/bitterKola/MultipleOfTheOther.java
@@ -8,14 +8,14 @@ public class MultipleOfTheOther{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.printf("Enter value for first number: ");
-        double firstNumber = input.nextDouble();
+        int firstNumber = input.nextInt();
         System.out.printf("Enter value for secondNumber: ");
-        double secondNumber = input.nextDouble();
+        int secondNumber = input.nextInt();
         if (firstNumber % secondNumber == 0){
-            System.out.printf("Yes, the first number %.2f is a multiple of the second number %.2f%n", firstNumber, secondNumber);
+            System.out.printf("Yes, the first number %d is a multiple of the second number %d%n", firstNumber, secondNumber);
         }
         else{
-            System.out.printf("No, the first number %.2f is NOT a multiple of the second number %.2f.%n", firstNumber, secondNumber);    
+            System.out.printf("No, the first number %d is NOT a multiple of the second number %d%n", firstNumber, secondNumber);    
         }
     }
 }
