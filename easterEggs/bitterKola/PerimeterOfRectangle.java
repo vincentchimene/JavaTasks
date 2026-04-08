c4505d5dcc0b808e89fa81c0e0b5da1d3aa3d214
Vincent Opurum <vincentchimene@gmail.com>
2026-04-08 08:29:48 +0100

    adds WeightCost task

---
+++ easterEggs/bitterKola/PerimeterOfRectangle.java
@@ -0,0 +1,7 @@
+/*
+* A Java program that reads two edges of a rectangle and 
+* computes the perimeter if the input is valid.
+* The input is valid only if both edges are of different lengths.
+* Otherwise, display that the input is invalid.
+*/
+
