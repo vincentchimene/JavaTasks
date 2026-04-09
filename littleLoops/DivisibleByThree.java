f8a434d56105905199d413770492692f43f547fd
Vincent Opurum <vincentchimene@gmail.com>
2026-04-09 01:28:59 +0100

    Adds DivisibiltyByThree tasks

---
+++ littleLoops/DivisibleByThree.java
@@ -0,0 +1,15 @@
+/*
+* A java program that prints numbers divisible by 3 between 1 and 30.
+*/
+
+
+
+public class DivisibleByThree{
+    public static void main(String[] args){
+        for (int i = 3; i <= 30; i+=3)
+            System.out.printf("%d%n", i);
+
+    }
+
+}
+
