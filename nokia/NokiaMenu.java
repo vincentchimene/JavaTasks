5d4daa8ff819e67577dc2cd2874910b3f1d04b3a
Vincent Opurum <vincentchimene@gmail.com>
2026-04-15 07:44:42 +0100

    adds functionality for Messages Menu

--- nokia/NokiaMenu.java
+++ nokia/NokiaMenu.java
@@ -228,7 +228,288 @@ public class NokiaMenu{
                 break;
                 
             case 2: // L1 MESSAGES
+                
                 System.out.printf("MESSAGES %n");
+                while(true){
+                    System.out.printf("%n        MESSAGES      %n");
+                    System.out.printf("1. Write Messages%n2. Inbox%n3. Outbox name%n4. Picture Message%n5. Templates%n6. Simileys%n7. Message Settings%n8. Info Service%n9. Voice Mail Number%n10. Service Command Editor%n0. Back%n");
+                    int subMenuChoice = input.nextInt();
+                    switch (subMenuChoice){
+                    
+                    case 1: // L2   WRITE MESSAGES
+                        while(true){
+                            System.out.println("%n      WRITE MESSAGES       %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        }
+                        break;
+
+                    case 2: // L2   INBOX
+                        while(true){
+                            System.out.println("%n      INBOX     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 3: //  L2  OUTBOX
+                        while(true){
+                            System.out.println("%n      OUTBOX     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 4: //  L2  PICTURE MESSAGE
+                        while(true){
+                            System.out.println("%n      PICTURE MESSAGE     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 5: //  L2  TEMPLATES
+                        while(true){
+                            System.out.println("%n      TEMPLATES     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 6: //  L2  SIMILEYS
+                        while(true){
+                            System.out.println("%n      SIMILEYS     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 8: //  L2  INFO SERVICE
+                        while(true){
+                            System.out.printf("%n      INFO SERVICE     %n");
+                            System.out.printf("0. Back%n");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                   case 7: // L2    MESSAGE SETTINGS
+                        while(true) {
+                            System.out.printf("%n      MESSAGE SETTINGS     %n");
+                            System.out.printf("1. Set 1%n2. Common%n0. Back%n");
+
+                            int messageSettingsMenuChoice = input.nextInt();
+
+                            switch (messageSettingsMenuChoice){
+
+                                case 1: // L3   SET 1
+                                    while(true) {
+                                        System.out.printf("%n      SET 1     %n");
+                                        System.out.printf("1. Message Center Number%n2. Messages Sent as%n3. Message Validity%n0. Back%n");
+
+                                        int set1MenuChoice = input.nextInt();
+                                        switch(set1MenuChoice){
+                                            case 1: // L4 Message center Number
+                                                while(true){
+                                                    System.out.printf("%n       Message Center Number       %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int messageCenterNumberMenuChoice = input.nextInt();
+                                                    if (messageCenterNumberMenuChoice == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 2: // L4 Messages Sent as
+                                                while(true){
+                                                    System.out.printf("%n       Messages Sent as       %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int messagesSentAsMenuChoice = input.nextInt();
+                                                    if (messagesSentAsMenuChoice == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 3: // L4 Message Validity
+                                                while(true){
+                                                    System.out.printf("%n       Message Validity       %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int messageValidityMenuChoice = input.nextInt();
+                                                    if (messageValidityMenuChoice == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 0:
+                                                break;
+                                            default:
+                                                System.out.printf("Invalid entry choice!%n");       
+                                        }
+                                       if (set1MenuChoice == 0) break;
+                                    }
+                                    break;
+
+                                case 2: // L3   COMMON
+                                    while(true) {
+                                        System.out.printf("%n      COMMON    %n");
+                                        System.out.printf("1. Delivery Reports%n2. Reply Via Same Center%n3. Character Support%n0. Back%n");
+
+                                        int commonMenuChoice = input.nextInt();
+                                        switch(commonMenuChoice){
+                                            case 1: // L4 Delivery Reports
+                                                while(true){
+                                                    System.out.printf("%n       Delivery Reports      %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int deliveryReportsMenuChoice = input.nextInt();
+                                                    if (deliveryReportsMenuChoice == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 2: // L4 Reply Via Same Center
+                                                while(true){
+                                                    System.out.printf("%n       Reply Via Same Center      %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int replyViaSameCenter = input.nextInt();
+                                                    if (replyViaSameCenter == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 3: // L4 Character Support
+                                                while(true){
+                                                    System.out.printf("%n       Character Support       %n");
+                                                    System.out.printf("%n0. Back%n");
+
+                                                    int characterSupportMenuChoice = input.nextInt();
+                                                    if (characterSupportMenuChoice == 0) break; 
+                                                    else System.out.printf("Invalid entry choice!%n");                 
+                                                }
+                                                break;
+                                            case 0:
+                                                break;
+                                            default:
+                                                System.out.printf("Invalid entry choice!%n");       
+                                        }
+                                       if (commonMenuChoice == 0) break;
+                                    }
+                                    break;
+
+                                case 0:
+                                    break;
+
+                                default:
+                                    System.out.printf("Invalid choice!%n");
+                            }
+
+                            if (messageSettingsMenuChoice == 0) break;
+                        }
+                        break;
+                                
+
+                    case 9: //  L2  SPEED DIALS
+                        while(true){
+                            System.out.println("%n      SPEED DIALS     %n");
+                            System.out.println("0. Back");
+
+                            int speedDialsMenuChoice = input.nextInt();
+                            switch (speedDialsMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (speedDialsMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 10: // L2  VOICE TAGS
+                        while(true){
+                            System.out.println("%n      VOICE TAGS     %n");
+                            System.out.println("0. Back");
+
+                            int subSubMenuChoice = input.nextInt();
+                            switch (subSubMenuChoice){
+                                case 0:
+                                    break;
+                                default:
+                                    System.out.printf("Invalid choice!%n"); 
+                
+                            }
+                            if (subSubMenuChoice == 0) break;
+                        } 
+                        break;  
+
+                    case 0://   L2  BACK
+                        break;                        
+
+                    default:
+                        System.out.printf("Invalid choice!%n");                                      
+                       }        
+                   if (subMenuChoice == 0) break;        
+                }
                 break;
             
 
