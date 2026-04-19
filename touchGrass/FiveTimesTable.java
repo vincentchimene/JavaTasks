2ac7f4acd5676a3e41887fc9940f43a9d8467a22
Vincent Opurum <vincentchimene@gmail.com>
2026-04-19 16:20:37 +0100

    Adds multiplication table of 5 task

---
+++ touchGrass/FiveTimesTable.java
@@ -0,0 +1,13 @@
+/**
+*3. Write a program that prints the multiplication table of 5.(from 1x to 12x).
+*
+*/
+
+public class FiveTimesTable{
+    public static void main(String[] args){
+        for (int count = 1; count <=12; count++){
+            System.out.printf("%n5 X %d  =  %d%n", count, count*5);
+        } 
+    }
+
+}
