b9a47eb745f0cfca93c6898fc2ad30e53af1dde4
Vincent Opurum <vincentchimene@gmail.com>
2026-05-02 18:18:22 +0100

    Adds methods and test for getFlowEndDate

--- mentrualApp/MenstrualAppTest.java
+++ mentrualApp/MenstrualAppTest.java
@@ -12,17 +12,21 @@ public class MenstrualAppTest{
     @Test
     public void testForFlowStartDateCalculation(){
         MenstrualApp calc = new MenstrualApp();
-        LocalDate lastPeriod = LocalDate.of(2026, 4, 20);
-        LocalDate result = calc.getFlowStartDate(lastPeriod);
+        LocalDate lastPeriodStartDate = LocalDate.of(2026, 4, 20);
+        LocalDate result = calc.getFlowStartDate(lastPeriodStartDate);
         LocalDate expected = LocalDate.of(2026, 4, 20);
         assertEquals(expected, result);
         
     } 
     
-    //@Test
-    //public void testThatFlowEndDateIsLastPeriodDatePlusPeriodDuration(){
-             
-   // }
+    @Test
+    public void testThatFlowEndDateIsLastPeriodDatePlusPeriodDuration(){
+        MenstrualApp calc = new MenstrualApp();
+        LocalDate lastPeriodStartDate = LocalDate.of(2026, 4, 20);
+        int periodDuration = 5;
+        LocalDate result = calc.getFlowEndDate(lastPeriodStartDate, periodDuration);
+        LocalDate expected = LocalDate.of(2026, 4, 25);                  
+    }
 
 
 }
